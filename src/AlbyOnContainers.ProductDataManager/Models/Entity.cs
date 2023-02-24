959b1f43bab83a406de0814efe04917f96e5d323
Alberto Viezzi <aviezzi@outlook.com>
2023-02-24 21:01:52 +0100

    Temp

--- src/AlbyOnContainers.ProductDataManager/Models/Entity.cs
+++ src/AlbyOnContainers.ProductDataManager/Models/Entity.cs
@@ -3,7 +3,7 @@ namespace AlbyOnContainers.ProductDataManager.Models;
 using System;
 using System.ComponentModel.DataAnnotations;
 
-public abstract class Entity
+public abstract class Entity :  IEquatable<Entity>
 {
     [Key]
     [Required]
@@ -32,4 +32,24 @@ public abstract class Entity
     [Required]
     [ConcurrencyCheck]
     public string LastModifiedBy  { get; set; }
+
+    public bool Equals(Entity other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Id.Equals(other.Id);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Entity)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
 }
